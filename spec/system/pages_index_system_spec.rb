fc26dc3b81a1d58770516113f3af0aa8bb0e5382
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-05-16 15:50:34 -0300

    Retomando o projeto

--- spec/system/pages_index_system_spec.rb
+++ spec/system/pages_index_system_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe 'Exchange Currency Process', type: :system, js: true do
       select('USD', from: 'target_currency')
       fill_in 'amount', with: '10'
     end
+
     click_button 'CONVERTER'
 
     # save_and_open_page
